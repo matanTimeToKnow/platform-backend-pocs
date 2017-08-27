9233d5333ddfc5cb95097893bbd1bff2b4baaca0
matan.ochayon <matan.ochayon@timetoknow.com>
2017-08-27 13:07:02 +0300

    use spring-cloud-aws

---
+++ test-spring-cloud-aws/src/main/java/org/springframework/cloud/aws/sample/SqsTestApplication.java
@@ -0,0 +1,29 @@
+
+package org.springframework.cloud.aws.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan
+@PropertySource(value="classpath:aws-access.properties")
+public class SqsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqsTestApplication.class, args);
+    }
+
+
+    //private static final String QUEUE_NAME = "ASSETS_ON_FILE_UPLOAD_QUEUE";
+
+    @SqsListener(value = "${queue.name}")
+    private void receiveMessage(String message) {
+        System.out.println("");
+    }
+
+}
